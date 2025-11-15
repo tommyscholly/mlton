8a9c015865e87cc83a9ebd6b2ea3147959428fc1
Tom Schollenberger <tss2344@g.rit.edu>
2025-11-14 20:37:50 -0500

    remove die from runtime

--- runtime/gc/regions.c
+++ runtime/gc/regions.c
@@ -1,4 +1,6 @@
 void GC_regionPush (GC_state s) {
+  // printf("Region pushing\n");
+
   struct RegionStackNode* node = (struct RegionStackNode*) malloc(sizeof(struct RegionStackNode));
   if (node == NULL)
     die ("Cannot allocate region stack node");
@@ -8,8 +10,17 @@ void GC_regionPush (GC_state s) {
 }
 
 void GC_regionPop (GC_state s) {
-  if (s->regionStack == NULL)
-    die ("Region stack underflow");
+  // printf("Region popping\n");
+
+  if (s->regionStack == NULL) {
+    printf ("Region stack underflow");
+    return;
+  }
+
+  if (s->regionTop > s->regionStack->partitionStart) {
+    printf("We allocated something\n");
+  }
+
   s->regionTop = s->regionStack->partitionStart;
   struct RegionStackNode* temp = s->regionStack;
   s->regionStack = s->regionStack->next;
